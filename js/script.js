fe8b25bab6cd499c43c40ebdff6319d950656c59
ShapedShadows <88666162+ShapedShadows@users.noreply.github.com>
2021-08-11 19:01:29 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -17,3 +17,21 @@ function love(){
 function unlove() {
     document.getElementById("love").style.display = "none";
 }
+
+var countDownDate = new Date("Aug 15, 2021 21:00:00").getTime();
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+    
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("timer").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+    
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "NEW ALBUM IS PUBLISHED";
+  }
+}, 1000);
